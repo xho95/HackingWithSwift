ac0afc2027a0108b96408a57804a3238573fda67
Kim, Min Ho <xho1995@gmail.com>
2021-07-24 15:46:41 +0900

    add a new project for the triggering events

---
+++ EventTrigger/README.md
@@ -0,0 +1,4 @@
+#  Project
+
+* Reference : [Triggering events repeatedly using a timer](https://www.hackingwithswift.com/books/ios-swiftui/triggering-events-repeatedly-using-a-timer)
+
---
+++ EventTrigger/Shared/EventTriggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventTriggerApp.swift
+//  Shared
+//
+//  Created by Kim, Min Ho on 2021/07/24.
+//
+
+import SwiftUI
+
+@main
+struct EventTriggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
